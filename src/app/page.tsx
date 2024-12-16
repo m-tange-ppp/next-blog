c4cb72e72f947fe32515d7d10880a2edc73e4b40
MiTange <tmrevo210@yahoo.co.jp>
2024-12-17 01:38:09 +0900

    https://を忘れていたので追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import { PostType, AllBlogsType } from "./types";
 
-const apiUrl = process.env.NEXT_PUBLIC_VERCEL_URL;
+const apiUrl = `http://${process.env.NEXT_PUBLIC_VERCEL_URL}`;
 
 async function fetchAllBlogs() {
   const res = await fetch(`${apiUrl}/api/blog`, {
